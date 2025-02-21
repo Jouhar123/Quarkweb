73a08f1cea04da76bce0edb7f3e9c5f60b8f7172
jouhar123 <jouharparvez123@gmail.com>
2025-02-21 15:57:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript for toggling the mobile menu
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('active');
+    sidebar.classList.toggle("active");
+});
